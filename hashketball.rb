cdf06cddf1656bbfc4fea0257dff8b2d897a42e6
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-09 00:33:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,9 +9,13 @@ def game_hash
     
     {:player_name => "Brook Lopez", :number => 11, :shoe => 17,:points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15},
     
+<<<<<<< HEAD
     {:player_name => "Mason Plumlee", :number => 1, :shoe => 19,:points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5},
     
     {:player_name => "Jason Terry", :number => 31, :shoe => 15,:points => 19, :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1}
+=======
+    {:player_name => "Mason Plumlee", :number => 1, :shoe => 19,:points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5}
+>>>>>>> 2a587a6f07ec397574f76b358f3eba04ce0da93e
   ]}, 
     :away => {:team_name => 'Charlotte Hornets', :colors => ["Turquoise", "Purple"], :players => [
     {:player_name => "Jeff Adrien", :number => 4, :shoe => 18,:points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
@@ -20,6 +24,7 @@ def game_hash
     
     {:player_name => "DeSagna Diop", :number => 2, :shoe => 14,:points => 24, :rebounds => 12, :assists => 12, :steals => 4, :blocks => 5, :slam_dunks => 5},
     
+<<<<<<< HEAD
     {:player_name => "Ben Gordon", :number => 8, :shoe => 15,:points => 33, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0},
     
     {:player_name => "Kemba Walker", :number => 33, :shoe => 15,:points => 6, :rebounds => 12, :assists => 12, :steals => 7, :blocks => 5, :slam_dunks => 12}
@@ -27,4 +32,11 @@ def game_hash
   
   
   # binding.pry
+=======
+    {:player_name => "Ben Gordon", :number => 8, :shoe => 15,:points => 33, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0}
+  ]}}
+  
+  
+  binding.pry
+>>>>>>> 2a587a6f07ec397574f76b358f3eba04ce0da93e
 end
